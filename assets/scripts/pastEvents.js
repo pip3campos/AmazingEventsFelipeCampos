1481716b860989acfd623de17b37a53a2fb70190
pip3campos <felipeacampose@gmail.com>
2023-08-14 07:53:01 -0400

    card error de busqueda

--- assets/scripts/pastEvents.js
+++ assets/scripts/pastEvents.js
@@ -68,5 +68,18 @@ contenedorFiltro.addEventListener( 'input', suceso => {
     const filtradoCategorias = filtrarPorCategoria ( data.events, arrayChecked)
     const input = document.getElementById('searchEvento')
     const filtradoNombre = filtrarPorNombre ( filtradoCategorias, input.value)
-    mostrarTarjeta ( filtradoNombre, contenedor )
+    if ( filtradoNombre.length == 0 ){
+        contenedor.innerHTML = `<section class="card col-11 col-sm-3">
+        <img class="card-img-top img-fluid" src="../images/error_search.png" alt="">
+        <div class="card-body d-flex flex-column">
+            <h2 class="card-title">No results found</h2>
+            <p class="card-text">We couldn't find what you searched for.</p>
+            <div class="d-flex justify-content-around align-items-center">
+                <h3 class="card-title">Try searching again</h3>
+            </div>
+        </div>
+    </section>`
+    } else {
+        mostrarTarjeta ( filtradoNombre, contenedor )
+    }
 })
